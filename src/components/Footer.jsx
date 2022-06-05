8fbc1c3bf9beac69a23d025a02c7b6f742683afa
='Johnson-ad <=>
2022-06-05 13:58:41 +0000

    fin de creation du footer et debut creation de home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <div className='socialMedia'>
+            <FacebookIcon/>
+            <TwitterIcon/>
+            <InstagramIcon/>
+            <LinkedInIcon/>
+        </div>
+        <p> &copy; 2022 Johnson-ad</p>
+    </div>
   )
 }
 
