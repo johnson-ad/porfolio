df02edf69fe4e12c216d7c42d6af1fe7a0c531f2
='Johnson-ad <=>
2022-06-05 15:45:25 +0000

    Fin de creation de home page et debut creation de experience page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react'; 
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
+import GithubIcon from '@material-ui/icons/GitHub';
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className="about">
+          <h2>Hi, My Name is Johnson</h2>
+          <div className="prompt">
+            <p>A software developer with a passion for learning and creating.</p>            
+            <LinkedInIcon/>
+            <EmailIcon/>
+            <GithubIcon/>
+          </div>
+        </div>
+        <div className="skills">
+          <h1>Skills</h1>
+          <ol className="list">
+            <li className="item">
+              <h2>Front-End</h2>
+              <span>
+                NextJS, ReactJS, RxJS, Redux toolkit, React Native, HTML, CSS, Sass,  NPM, BootStrap, MateriaUI, Yarn, StyledComponents
+              </span>
+            </li>
+            <li className="item">
+              <h2>Back-End</h2>
+              <span>NodeJS, ExpressJs, MongoDB, GraphSQ, AWS S3</span>
+            </li>
+            <li className="item">
+              <h2> Languages </h2>
+              <span>Java script, Python, c#, Java, TypeScript</span>
+            </li>
+          </ol>
+        </div>
+    </div>
   )
 }
 
