b02c79c86281ea6430dc6ffc6d88e3357e2a815b
='Johnson-ad <=>
2022-06-05 17:32:21 +0000

    Creation des liens des differentes liens de chaque project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem';
+import "../styles/Projects.css";
+import  {ProjectList} from "../helpers/ProjectList"
+
 
 function Projects() {
   return (
     <div className='projects'>
       <h1>My personal Projects</h1>
-      <div className="projecList"></div>
+      <div className="projectList">
+        {ProjectList.map((project)=>{
+          return <ProjectItem  name={project.name} image={project.image}/>
+        })}
+      </div>
     </div>
   )
 }
