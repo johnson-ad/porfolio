97e69feeda8300a748a987ca524fe39b4d1b4c1a
='Johnson-ad <=>
2022-06-05 16:54:05 +0000

    Debut de la creation de la page project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <h1>My personal Projects</h1>
+      <div className="projecList"></div>
+    </div>
   )
 }
 
